e5ce6192fc31305da393c9e9ecc9c4d26baa9194
João Victor <jvaprince@gmail.com>
2026-07-04 19:54:04 -0300

    fix onesignal permission request

--- src/components/notifications/OneSignalInit.tsx
+++ src/components/notifications/OneSignalInit.tsx
@@ -6,25 +6,30 @@ import { createSupabaseClient } from '@/lib/supabase/client'
 
 export default function OneSignalInit() {
   useEffect(() => {
-    async function initOneSignal() {
+    async function init() {
+      await OneSignal.init({
+        appId: '85458890-7ca0-46f0-8c8b-7cc98328c64f',
+        allowLocalhostAsSecureOrigin: true,
+      })
+
       const supabase = createSupabaseClient()
 
       const {
         data: { user },
       } = await supabase.auth.getUser()
 
-      await OneSignal.init({
-        appId: '85458890-7ca0-46f0-8c8b-7cc98328c64f',
-        allowLocalhostAsSecureOrigin: true,
-      })
-
       if (user?.id) {
         await OneSignal.login(user.id)
-        await OneSignal.User.PushSubscription.optIn()
+
+        const permission = await OneSignal.Notifications.permission
+
+        if (!permission) {
+          await OneSignal.Notifications.requestPermission()
+        }
       }
     }
 
-    initOneSignal()
+    init()
   }, [])
 
   return null
